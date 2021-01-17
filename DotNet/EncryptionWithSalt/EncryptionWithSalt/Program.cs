5956ab310764be8d83f3bb1e0c4223c13fad9078
Lucas Alcantara <lucashmalcantara@gmail.com>
2021-01-17 13:49:34 -0300

    Implementa criptografia com .NET

---
+++ DotNet/EncryptionWithSalt/EncryptionWithSalt/Program.cs
@@ -0,0 +1,40 @@
+﻿using EncryptionWithSalt.Services;
+using System;
+
+namespace EncryptionWithSalt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ShowHeader();
+            DoEncrypt();
+            Console.ReadKey();
+        }
+
+        private static void DoEncrypt()
+        {
+            const string someText = "Hello! My name is Lucas.";
+            
+            const uint key = 1597534560;
+            var saltPositions = new int[] { 2, 3, 5, 7, 11 };
+            var transformStringsService = new TransformStringsService(key, saltPositions);
+
+            const string randomNumbers = "9563216516215646516516518967";
+            var encodedText = transformStringsService.Encode(someText, randomNumbers);
+            var decodedText = transformStringsService.Decode(encodedText, randomNumbers);
+
+            Console.WriteLine($"Text to encrypt: {someText}");
+            Console.WriteLine($"Private Key used to encrypt: {key}");
+            Console.WriteLine($"SALT Positions (used to build the Final Key): {string.Join(',', saltPositions)}");
+            Console.WriteLine($"Encoded text: {encodedText}");
+            Console.WriteLine($"Decoded text: {decodedText}");
+        }
+
+        private static void ShowHeader()
+        {
+            Console.WriteLine("=== Encryption With Salt ===");
+            Console.WriteLine("Show how to encrypt a string with obfuscation using XOR + SALT technique.\r\n");
+        }
+    }
+}
---
+++ DotNet/EncryptionWithSalt/EncryptionWithSalt/Services/TransformStringsService.cs
@@ -0,0 +1,90 @@
+﻿using EncryptionWithSalt.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace EncryptionWithSalt.Services
+{
+    public class TransformStringsService : ITransformStringsService
+    {
+        private readonly Random _random = new Random();
+
+        public uint Key { get; private set; }
+        public IEnumerable<int> SaltPositions { get; private set; }
+
+        public TransformStringsService(uint key, IEnumerable<int> saltPositions)
+        {
+            Key = key;
+            SaltPositions = saltPositions;
+        }
+        public string Decode(string text, string salt)
+        {
+            var fromBase64 = Convert.FromBase64String(text);
+            var unescaped = Unescape(fromBase64);
+
+            var flipped = Flip(unescaped, salt);
+            return Encoding.UTF8.GetString(flipped, 0, flipped.Length);
+        }
+
+        public string Encode(string text, string salt)
+        {
+            var flipped = Flip(text, salt);
+            return Convert.ToBase64String(flipped);
+        }
+
+        public string GenerateRandomSalt(int length = 15)
+        {
+            var randomSalt = new StringBuilder();
+
+            for (int i = 0; i < length; i++)
+                randomSalt.Append(
+                    _random.Next(maxValue: 10).ToString());
+
+            return randomSalt.ToString();
+        }
+
+        private uint CalculateFinalKey(string salt)
+        {
+            var finalKey = Key;
+
+            foreach (var position in SaltPositions)
+            {
+                var saltItem = uint.Parse(salt[position].ToString());
+                finalKey += saltItem;
+            }
+
+            return finalKey;
+        }
+
+        private string Unescape(byte[] fromBase64)
+        {
+            string textFromBase64 = Encoding.UTF8.GetString(fromBase64);
+            return Regex.Unescape(textFromBase64.Replace(@"\", @"\\"));
+        }
+
+        private byte[] Flip(string text, string salt)
+        {
+            var finalKey = CalculateFinalKey(salt);
+
+            var buffer = Encoding.UTF8.GetBytes(text);
+            var k1 = finalKey & 0xff;
+            var k2 = (finalKey >> 8) & 0xff;
+            var k3 = (finalKey >> 16) & 0xff;
+            var k4 = (finalKey >> 24) & 0xff;
+            var flipped = new byte[buffer.Length];
+
+            for (var i = 0; i < buffer.Length; i++)
+            {
+                var c = buffer[i];
+                var temp = c ^ k1;
+                temp ^= k2;
+                temp ^= k3;
+                temp ^= k4;
+                flipped[i] = (byte)temp;
+            }
+
+            return flipped;
+        }
+    }
+}
